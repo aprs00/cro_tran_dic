73315769c19e2db28f6045a781ab914874793778
Andrej Prskalo <aprskalo00@gmail.com>
2023-04-10 16:28:25 +0200

    now scraping definitions from vocabulary.com

--- main.py
+++ main.py
@@ -1,31 +1,29 @@
 import discord
 import requests
 import os
-from googletrans import Translator
+from bs4 import BeautifulSoup
+# from googletrans import Translator
+from google.cloud import translate_v2 as translate
 from dotenv import load_dotenv
-
-load_dotenv()
 
 intents = discord.Intents.all()
 intents.members = True
 
 client = discord.Client(intents=intents)
 
-DISCORD_CHANNEL_ID = os.getenv('DISCORD_CHANNEL_ID')
-CROTRANDIC_BOT_ID = os.getenv('CROTRANDIC_BOT_ID')
+load_dotenv()
 
 def get_definition_and_translation(word):
-    translator = Translator()
-    r = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word}')
-    data = r.json()
+    # translator = Translator()
+    translate_client = translate.Client()
 
-    if isinstance(data, dict) and data.get('title', None) == 'No Definitions Found':
-        return None, None, None, None, data['message']
+    translation_response = translate_client.translate(word, target_language='hr')
+    translation = translation_response['translatedText']
+
+    if translation == word:
+        return None, None, None, None, 'No translations found'
     
-    translation = translator.translate(word, dest='hr').text or None
-    definition = data[0].get('meanings', [{}])[0].get('definitions', [{}])[0].get('definition', None)
-    synonyms = data[0].get('meanings', [{}])[0].get('definitions', [{}])[0].get('synonyms', None)
-    example = data[0].get('meanings', [{}])[0].get('definitions', [{}])[0].get('example', None)
+    definition, synonyms, example = scrape_word_definition(word)
 
     return definition, translation, synonyms, example, None
 
@@ -34,8 +32,8 @@ async def send_invalid_word(word, channel, error_message):
     await word.delete(delay=5)
     await msg.delete(delay=5)
 
-def format_response(word, translation, definition, synonyms, example):
-    separator = '\n' + '-' * 50 + '\n'
+def format_response(word, translation, definition, example, synonyms):
+    separator = '\n' + '-' * 53 + '\n'
     response = ''
 
     if translation:
@@ -43,27 +41,69 @@ def format_response(word, translation, definition, synonyms, example):
     if definition:
         response += f'\n{definition}'
     if synonyms:
-        response += f'\n{", ".join(synonyms)}'
+        response += f'\n{synonyms}'
     if example:
         response += f'\n{example}'
 
     return response + separator
+
+async def delete_all_channel_messages(channel):
+    async for message in channel.history(limit=None):
+        await message.delete()
+    return
+
+def scrape_word_definition(word): 
+    url = f'https://www.vocabulary.com/dictionary/{word}'
+
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    print(soup)
+
+    synonyms, example = None, None
+    max_synonyms = 8
+
+    definition_exists = soup.find('div', {'class': 'definition'})
+    print(definition_exists)
+    if not definition_exists:
+        return None, None, None
+
+    definition_div = soup.find('div', {'class': 'definition'})
+    part_of_speech = definition_div.find('div', {'class': 'pos-icon'}).text.strip()
+    definition = definition_div.text.replace(part_of_speech, '').strip()
+
+    instances_div = soup.find('dl', {'class': 'instances'})
+    instances_detail = instances_div.find('span', {'class': 'detail'})
+    is_synonym = instances_detail.text.strip().lower() == 'synonyms:'
+    if is_synonym:
+        synonyms = ", ".join([s.text for s in instances_div.find_all('a')][:max_synonyms])
+
+    example_div = soup.find('div', {'class': 'example'})
+    if example_div:
+        example = example_div.text.replace('\n', '')
+
+    return definition, synonyms, example
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if str(message.channel.id) == DISCORD_CHANNEL_ID:
+    if str(message.channel.id) == os.getenv('DISCORD_CHANNEL_ID'):
         word = message.content
+
+        if (word == '!delete-all'):
+            await delete_all_channel_messages(message.channel)
+            return
+
         definition, translation, synonyms, example, error_message = get_definition_and_translation(word)
 
         if error_message:
             return await send_invalid_word(message, message.channel, error_message)
 
-        response = format_response(word, translation, definition, synonyms, example)
+        response = format_response(word, translation, definition, example, synonyms)
 
         await message.delete()
         await message.channel.send(response)
 
-client.run(CROTRANDIC_BOT_ID)
+client.run(os.getenv('CROTRANDIC_BOT_ID'))
